dc1e895edcb6d468ee86b9e627c88afa04f33fd7
Rick Pearce <rpearce63@yahoo.com>
2018-04-23 22:56:00 -0500

    use cocoapods to reload framework

--- ToDooey/AppDelegate.swift
+++ ToDooey/AppDelegate.swift
@@ -18,10 +18,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
 //        print(NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).last! as String)
-        print(Realm.Configuration.defaultConfiguration.fileURL)
+        //print(Realm.Configuration.defaultConfiguration.fileURL)
         
         do {
-            let realm = try Realm()
+            _ = try Realm()
         } catch {
             print(error)
         }
